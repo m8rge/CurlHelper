1a32ddaab5fcad9697afffb9fcfe8fa02bf7b250
m8rge <to.merge@gmail.com>
2015-12-18 17:34:18 +0500

    refactor

---
+++ src/CurlException.php
@@ -0,0 +1,30 @@
+<?php namespace m8rge;
+
+
+class CurlException extends \Exception
+{
+    /**
+     * @var CurlResult
+     */
+    public $curlResult;
+
+    public $postFields;
+
+    /**
+     * @param CurlResult $curlResult
+     * @param array|null $postFields
+     * @param \Exception|null $previous
+     */
+    public function __construct($curlResult, $postFields = null, \Exception $previous = null)
+    {
+        $this->curlResult = $curlResult;
+        $this->postFields = $postFields;
+        if ($curlResult->error) {
+            $message = ($postFields ? 'posting to' : 'retrieving') . " url $curlResult->url failed with error: $curlResult->error";
+        } elseif ($curlResult->statusCode >= 400) {
+            $message = ($postFields ? 'posting to' : 'retrieving') . " url $curlResult->url return $curlResult->statusCode response code";
+        }
+
+        parent::__construct(isset($message) ? $message : '', 0, $previous);
+    }
+}
---
+++ src/CurlResult.php
@@ -0,0 +1,78 @@
+<?php namespace m8rge;
+
+
+class CurlResult
+{
+    /**
+     * @var string
+     */
+    public $statusCode;
+
+    /**
+     * @var string
+     */
+    public $url;
+
+    /**
+     * @var string
+     */
+    public $error;
+
+    /**
+     * @var string
+     */
+    public $response;
+
+    /**
+     * @var string[][]
+     */
+    public $headers;
+
+    /**
+     * CurlResult constructor.
+     * @param resource $curlHandler
+     */
+    public function __construct($curlHandler)
+    {
+        if (curl_errno($curlHandler)) {
+            $this->error = curl_error($curlHandler);
+        }
+
+        $response = curl_multi_getcontent($curlHandler);
+        if (is_string($response) && !empty($response)) {
+            list($headers, $body) = explode("\r\n\r\n", $response, 2);
+            $this->headers = $this->parseHeaders($headers);
+            $this->response = $body;
+        }
+        $this->statusCode = curl_getinfo($curlHandler, CURLINFO_HTTP_CODE);
+        $this->url = curl_getinfo($curlHandler, CURLINFO_EFFECTIVE_URL);
+    }
+
+    protected function parseHeaders($rawHeaders)
+    {
+        $headers = [];
+        $key = '';
+
+        foreach (explode("\n", $rawHeaders) as $headerLine) {
+            if (strpos($headerLine, ':') !== false) {
+                list($name, $value) = explode(':', $headerLine, 2);
+                $key = $name;
+
+                $trimmedValue = trim($value);
+                if (!isset($headers[$name])) {
+                    $headers[$name] = [$trimmedValue];
+                } elseif (is_array($headers[$name])) {
+                    $headers[$name][] = $trimmedValue;
+                }
+            } elseif ($headerLine[0] == "\t") {
+                end($headers[$key]);
+                $index = key($headers[$key]);
+                $headers[$key][$index] .= "\r\n\t" . trim($headerLine);
+            } else {
+                $headers[] = trim($headerLine);
+            }
+        }
+
+        return $headers;
+    }
+}
